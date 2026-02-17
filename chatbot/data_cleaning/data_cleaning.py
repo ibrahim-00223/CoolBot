2ce1d0f1f0731beecf9b626b1f710a696ab91079
ibrahim-00223 <ibrahim.kante.pro@gmail.com>
2026-02-17 22:10:33 +0100

    Added retriever and RAG configuration

--- chatbot/data_cleaning/data_cleaning.py
+++ chatbot/data_cleaning/data_cleaning.py
@@ -5,13 +5,17 @@ from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-from langchain_mistralai import MistralAIEmbeddings
+from langchain_mistralai import MistralAIEmbeddings, ChatMistralAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+
 
 load_dotenv()
 
 # STEP 1 : 
 
-files_path="sources"
+files_path="./sources"
     # Get all files in Folder "Sources" : 
 all_files = []
 pdf_files = list(Path(files_path).glob("*.pdf"))
@@ -61,5 +65,22 @@ vector_store = Chroma.from_documents(
 )
 query = "Je suis sur 30WG et j'aimerai savoir que signifie l'alarme 15001 et quelle est la cause éventuelle ?"
 results = vector_store.similarity_search(query, k=3)
-print(results)
-print("Hello World")
+
+retriever = vector_store.as_retriever(
+    search_type="similarity",  # ou "mmr"
+    search_kwargs={"k": 5}
+)
+
+# Retriever : 
+docs = retriever.invoke("Je suis sur un 30WG et j'aimerai savoir que signifie l'alarme 15001 ?")
+
+# Avec filtres
+retriever = vector_store.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={
+        "k": 10,
+        "score_threshold": 0.5  # Seuil minimum
+    }
+)
+
+print(retriever)
